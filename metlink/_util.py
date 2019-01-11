d2eda12c5780e68b1582c0d96616dc722c125a72
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-01-11 21:35:36 +1300

    DRY

--- metlink/_util.py
+++ metlink/_util.py
@@ -5,9 +5,10 @@ from datetime import timedelta, datetime
 def _format_time(t):
     """Create times that match those seen on the street signs."""
     ret = datetime.strptime(t[0:16], '%Y-%m-%dT%H:%M')
+    delta = timedelta(hours=int(t[19:22]), minutes=int(t[23:]))
     if t[18] == '+':
-        ret += timedelta(hours=int(t[19:22]), minutes=int(t[23:]))
+        ret += delta
     elif t[18] == '-':
-        ret -= timedelta(hours=int(t[19:22]), minutes=int(t[23:]))
+        ret -= delta
 
     return ret.strftime("%I:%M%p %a")
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='metlinkt',
+    version='0.1',
+    author="Brenda Wallace",
+    author_email="brenda@brenda.nz",
+    description="A wrapper for Metlink's API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/br3nda/metlink-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
